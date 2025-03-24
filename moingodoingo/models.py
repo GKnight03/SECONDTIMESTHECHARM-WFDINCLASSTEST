fd5f1821717a9a044a78df7ad402af7ccddd82ca
Josh Treacy <113988595+GKnight03@users.noreply.github.com>
2025-03-24 14:53:43 +0000

    Added Customer

--- moingodoingo/models.py
+++ moingodoingo/models.py
@@ -8,3 +8,16 @@ class Car(models.Model):
     colour = models.CharField(max_length=30)
     year = models.IntegerField()
     for_sale = models.BooleanField()
+
+class Customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=20)
+    address = models.TextField()
+    city = models.CharField(max_length=50)
+    state_province = models.CharField(max_length=20)
+    country = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=20)
+    
+    
